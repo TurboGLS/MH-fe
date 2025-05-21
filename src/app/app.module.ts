c77544327cc71f54649ee33a4395a5381ff778e6
TurboGLS <matteazzitomas@gmail.com>
2025-05-21 15:41:11 +0200

    fix + added some register in pv and multimetri

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,13 +13,13 @@ import { MultimetriPageComponent } from './pages/multimetri/multimetri-page.comp
 import { PAC2200Component } from './pages/multimetri/pac2200/pac2200.component';
 import { P20A000TComponent } from './pages/multimetri/p20-a000-t/p20-a000-t.component';
 import { P21A000TComponent } from './pages/multimetri/p21-a000-t/p21-a000-t.component';
-import { MFD4ORFCDT1Component } from './pages/multimetri/mfd4-orfcdt1/mfd4-orfcdt1.component';
 import { SXMT63Component } from './pages/multimetri/sxmt63/sxmt63.component';
 import { FotovoltaiciPageComponent } from './pages/fotovoltaici/fotovoltaici-page.component';
 import { STP3456Component } from './pages/fotovoltaici/stp3456/stp3456.component';
 import { STP810Component } from './pages/fotovoltaici/stp810/stp810.component';
 import { STP11060Component } from './pages/fotovoltaici/stp110-60/stp110-60.component';
 import { STP50Component } from './pages/fotovoltaici/stp-50/stp-50.component';
+import { SXMM63Component } from './pages/multimetri/sxmm63/sxmm63.component';
 
 @NgModule({
   declarations: [
@@ -31,13 +31,13 @@ import { STP50Component } from './pages/fotovoltaici/stp-50/stp-50.component';
     PAC2200Component,
     P20A000TComponent,
     P21A000TComponent,
-    MFD4ORFCDT1Component,
     SXMT63Component,
     FotovoltaiciPageComponent,
     STP3456Component,
     STP810Component,
     STP11060Component,
     STP50Component,
+    SXMM63Component,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/multimetri/sxmm63/sxmm63.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sxmm63',
+  standalone: false,
+  templateUrl: './sxmm63.component.html',
+  styleUrl: './sxmm63.component.scss'
+})
+export class SXMM63Component {
+
+}
