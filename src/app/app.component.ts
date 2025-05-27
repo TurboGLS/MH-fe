93e581e1a5f870b08ca7b2e9f870836e74106ea9
TurboGLS <matteazzitomas@gmail.com>
2025-05-27 09:46:22 +0200

    deleted ping

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { environment } from '../environments/environment';
 import { map } from 'rxjs';
-import { TestService } from './services/test-service';
 
 @Component({
   selector: 'app-root',
@@ -11,18 +10,4 @@ import { TestService } from './services/test-service';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  protected testSrv = inject(TestService);
-  risposta: string = '';
-
-   ngOnInit() {
-    this.testSrv.ping().pipe(
-      map(res => res.message)
-    ).subscribe({
-      next: res => {  
-        console.log('Risposta backend:', res);
-        this.risposta = res;
-      },
-      error: err => console.error('Errore chiamata:', err)
-    });
-  }
 }
