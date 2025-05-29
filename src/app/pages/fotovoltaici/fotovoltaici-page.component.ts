8511f924df7525f6ee6d85ef8daf4e83d298ce3a
TurboGLS <matteazzitomas@gmail.com>
2025-05-29 15:24:59 +0200

    keep elements with refresh page

--- src/app/pages/fotovoltaici/fotovoltaici-page.component.ts
+++ src/app/pages/fotovoltaici/fotovoltaici-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-fotovoltaici-page',
@@ -6,11 +7,30 @@ import { Component, Input } from '@angular/core';
   templateUrl: './fotovoltaici-page.component.html',
   styleUrl: './fotovoltaici-page.component.scss'
 })
-export class FotovoltaiciPageComponent {
+export class FotovoltaiciPageComponent implements OnInit {
+  protected router = inject(Router);
+  protected activatedRoute = inject(ActivatedRoute);
+
   @Input()
   selectedMulti: string | null = null;
 
+  ngOnInit(): void {
+      // Legge il parametro multi dall'URL
+      this.activatedRoute.queryParams.subscribe(params => {
+        if (params['multi']) {
+          this.selectedMulti = params['multi'];
+        }
+      });
+  }
+
   selectMulti(item: string) {
     this.selectedMulti = item;
+
+    // Aggiorna l'URL senza ricaricare la pagina
+    this.router.navigate([], {
+      relativeTo: this.activatedRoute,
+      queryParams: { multi: item },
+      queryParamsHandling: 'merge' // mantiene gli altri parametri
+    });
   }
 }
